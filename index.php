a9523e644868c5d1208924e670c5f1be579896dd
GILBERTO RAMOS DE OLIVEIRA <16254850@CFP127ALUNO.LOCAL>
2019-04-04 10:16:49 -0300

    Possivel solução

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
 
-    require_once("view/home.php");
-    
+    //require_once("view/home.php");
+	if(count($_GET)>=1){
+
+		$nome_pagina = array_keys($_GET)[0];
+
+			
+		//echo "Caminho : ". 'view/'.$nome_pagina.'.php </br>';
+		$pagina = 'view/'.$nome_pagina.'.php';
+		$exists = file_exists( $pagina );
+		
+		if($exists){
+			
+			require_once($pagina);
+			
+		}else{
+			
+			echo "Pagina não Existe!!";
+		
+		}
+
+	}else{
+		require_once("view/home.php");
+	}
 ?>
