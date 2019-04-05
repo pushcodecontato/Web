9b84b309c5c55f142caa9f3db88525ef3c2666e6
LARISSA BRUNA DE OLIVEIRA PEREIRA <17259209@CFP127ALUNO.LOCAL>
2019-04-05 08:22:36 -0300

    implantando o painel de usuario HOME

---
+++ view/painel_usuario/home.php
@@ -0,0 +1,284 @@
+<html>
+   
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>painel_usuario-Home</title>
+    <link rel="stylesheet" 
+          type="text/css"
+          href="view/painel_usuario/css/home.css"/>
+</head>
+    <body>
+        <!--DIV PRINCIPAL QUE SEGURA TUDO -->
+        <div id="principal">
+            <!--CABEÇALHO-->
+            <header>
+                <!--TODO CONTEUDO DO CABEÇALHO-->
+                <div id="conteudo_header">
+                     
+                           
+                    <div class="img_logo">
+                        <img class="img_logo" src="view/imagem/Mob.png" title="logo da empresa" alt="logo da empresa">
+                    </div>
+                    
+                    <div id="bemvindo">
+                        <h1> Bem vindo! </h1>
+                        <h4>Usuário</h4>
+                    
+                    </div>
+                    
+                   
+                    
+                    <div class="img_logo">
+                        <img class="img_usuario" src="view/painel_usuario/imagem/usuario.png" title="oto do usuario" alt="foto do usuario">
+                        
+                        
+                    </div>
+                    
+                    <!--Segura todos icones-->
+                    <div id="segura_icones"> 
+                        
+                        <div class="icone">
+                            
+                            <img class="img_icone" src="view/painel_usuario/imagem/icone_home.png" title="icone" alt="icone">
+                        
+                        </div>
+                        
+                        <div class="icone">
+                              <img class="img_icone" src="view/painel_usuario/imagem/icone_home.png" title="icone" alt="icone">
+                        
+                        </div>
+                        
+                        <div class="icone">
+                          <img class="img_icone" src="view/painel_usuario/imagem/icone_home.png" title="icone" alt="icone">
+                        </div>
+                        
+                        <div class="icone">
+                            <img class="img_icone" src="view/painel_usuario/imagem/icone_home.png" title="icone" alt="icone">
+                        
+                        </div>
+                    </div>
+                
+                </div>
+            </header>
+            <!--MENU-->
+            <nav>
+                <h5>Menu</h5>
+                
+                <div class="caixa_menu">
+                    <div class="menu_icone">
+                       <img class="menu_icone" src="view/painel_usuario/imagem/icone_home.png" title="icone" alt="icone">   
+                    </div>
+                    
+                    <!--titulos do menu-->
+                    <div class="menu_titulo">
+                        <p> 
+                            <a href="#">Gerenciar veiculos</a>
+                        
+                        </p>
+                    </div>
+                    
+                   
+                </div>
+                
+                <div class="caixa_menu">
+                    <div class="menu_icone">
+                        
+                        <img class="menu_icone" src="view/painel_usuario/imagem/icone_home.png" title="icone" alt="icone">
+                    </div>
+                    <!--titulos do menu-->
+                    <div class="menu_titulo">
+                        <p>
+                            <a href="#">Gerenciar anúncios </a>
+                        </p>
+                    </div>
+                    
+                </div>
+                
+                <div class="caixa_menu">
+                    <div class="menu_icone">
+                        <img class="menu_icone" src="view/painel_usuario/imagem/icone_home.png" title="icone" alt="icone">  
+                    </div>
+                    <!--titulos do menu-->
+                    <div class="menu_titulo">
+                        <p>
+                            <a href="#"> Gerenciar locações</a> 
+                        
+                        </p>
+                    </div>
+                    
+                </div>
+                
+                <div class="caixa_menu">
+                    <div class="menu_icone">
+                        <img class="menu_icone" src="view/painel_usuario/imagem/icone_home.png" title="icone" alt="icone">   
+                    </div>
+                    <!--titulos do menu-->
+                    <div class="menu_titulo">
+                        <p>
+                            <a href="#"> Conversas</a>
+                        </p>
+                    </div>
+                    
+                </div>
+            
+            </nav>
+            
+            <div id="conteudo">
+                <h2 id="h2Border">Interessados nos veículos</h2>
+                
+                
+                <div id="principal_anuncios">
+                    <!--parte de cima do anuncio-->
+                    <div class="segura_anuncio">
+                        
+                        <div class="caixa_anuncio">
+                            <div class="imagem_anuncio"> 
+                                
+                                <img class="foto_anuncio" src="view/painel_usuario/imagem/carro.jpg" title="icone" alt="icone">
+                            </div>
+                            
+                            <div class="titulo_anuncio">
+                                <h4> Titulo </h4>
+                            </div>
+                            
+                            <div class="dados_anuncio">
+                                <p id="dados"> 
+                                    <strong>Nome:</strong> Larissa Bruna<br><br>
+                                    <strong>Horário:</strong> 12h - 13h<br><br>
+                                    <strong>Valor previsto: </strong>R$120<br><br>
+                                    <strong>Avaliação:</strong> 8.9
+                                </p>
+                            </div>
+                        
+                        </div>
+                        
+                        <div class="caixa_anuncio">
+                        
+                            <div class="imagem_anuncio">
+                                <img class="foto_anuncio" src="view/painel_usuario/imagem/carro.jpg" title="icone" alt="icone">
+                            
+                            </div>
+                            
+                            <div class="titulo_anuncio">
+                                <h4> Titulo </h4>
+                            </div>
+                            
+                             
+                            <div class="dados_anuncio">
+                                <p id="dados"> 
+                                    <strong>Nome:</strong> Larissa Bruna<br><br>
+                                    <strong>Horário:</strong> 12h - 13h<br><br>
+                                    <strong>Valor previsto: </strong>R$120<br><br>
+                                    <strong>Avaliação:</strong> 8.9
+                                </p>
+                            </div>
+                        
+                        
+                        </div>
+                        
+                        <div class="caixa_anuncio">
+                            <div class="imagem_anuncio">
+                                <img class="foto_anuncio" src="view/painel_usuario/imagem/carro.jpg" title="icone" alt="icone">
+                            </div>
+                            
+                            <div class="titulo_anuncio">
+                                <h4> Titulo </h4>
+                            </div>
+                            
+                             
+                            <div class="dados_anuncio">
+                                <p id="dados"> 
+                                    <strong>Nome:</strong> Larissa Bruna<br><br>
+                                    <strong>Horário:</strong> 12h - 13h<br><br>
+                                    <strong>Valor previsto: </strong>R$120<br><br>
+                                    <strong>Avaliação:</strong> 8.9
+                                </p>
+                            </div>
+                        
+                        
+                        </div>
+                    
+                    
+                    </div>
+                    
+                    
+                    <!--parte de baixo do anuncio-->
+                     <div class="segura_anuncio">
+                        <div class="caixa_anuncio">
+                           <div class="imagem_anuncio">
+                                <img class="foto_anuncio" src="view/painel_usuario/imagem/carro.jpg" title="icone" alt="icone">
+                            </div>
+                            
+                            <div class="titulo_anuncio">
+                                <h4> Titulo </h4>
+                            </div>
+                            
+                             
+                            <div class="dados_anuncio">
+                                <p id="dados"> 
+                                    <strong>Nome:</strong> Larissa Bruna<br><br>
+                                    <strong>Horário:</strong> 12h - 13h<br><br>
+                                    <strong>Valor previsto: </strong>R$120<br><br>
+                                    <strong>Avaliação:</strong> 8.9
+                                </p>
+                            </div>
+                        
+                        
+                         
+                        </div>
+                        <div class="caixa_anuncio">
+                         
+                           <div class="imagem_anuncio">
+                                <img class="foto_anuncio" src="view/painel_usuario/imagem/carro.jpg" title="icone" alt="icone">
+                            </div>
+                            
+                            <div class="titulo_anuncio">
+                                <h4> Titulo </h4>
+                            </div>
+                            
+                             
+                            <div class="dados_anuncio">
+                                <p id="dados"> 
+                                    <strong>Nome:</strong> Larissa Bruna<br><br>
+                                    <strong>Horário:</strong> 12h - 13h<br><br>
+                                    <strong>Valor previsto: </strong>R$120<br><br>
+                                    <strong>Avaliação:</strong> 8.9
+                                </p>
+                            </div>
+                        
+                        
+                        </div>
+                        <div class="caixa_anuncio">
+                           <div class="imagem_anuncio">
+                                <img class="foto_anuncio" src="view/painel_usuario/imagem/carro.jpg" title="icone" alt="icone">
+                            </div>
+                            
+                            <div class="titulo_anuncio">
+                                <h4> Titulo </h4>
+                            </div>
+                            
+                             
+                            <div class="dados_anuncio">
+                                <p id="dados"> 
+                                    <strong>Nome:</strong> Larissa Bruna<br><br>
+                                    <strong>Horário:</strong> 12h - 13h<br><br>
+                                    <strong>Valor previsto: </strong>R$120<br><br>
+                                    <strong>Avaliação:</strong> 8.9
+                                </p>
+                            </div>
+                        
+                        
+                         
+                        </div>
+                    
+                    
+                    </div>
+                
+                
+                </div>
+            </div>
+            
+        </div>
+    </body>
+</html>
