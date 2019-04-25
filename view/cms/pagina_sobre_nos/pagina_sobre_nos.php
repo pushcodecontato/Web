a4843eb0c3a7136ae8ca676f912001020be44bf5
LARISSA BRUNA DE OLIVEIRA PEREIRA <17259209@CFP127ALUNO.LOCAL>
2019-04-25 11:26:24 -0300

    modificando o front do sobre

--- view/cms/pagina_sobre_nos/pagina_sobre_nos.php
+++ view/cms/pagina_sobre_nos/pagina_sobre_nos.php
@@ -2,105 +2,81 @@
 
 <!--div que segura -->
 <div id="principal">
-    <div id="borda">
-        <div id="h2borda"> 
-            Historia
+    <div class="segura_text_button">
+        
+        <h2>Tabelas Sobre a empresa</h2>
+        <div id="segura_button"> 
+            <button class="adicionar_sobre" id="abrir_cadastro">Adicionar Historia </button>
+            <button class="adicionar_sobre" id="abrir_cadastro">Adicionar missão visão Valores </button>
         </div>
         
-        <div class="conteudo_dentro">
-            <div class="informacoes">Titulo</div>
-            <div class="informacoes">Descrição</div>
-            <div class="informacoes">imagem</div>
-            <div class="informacoes">opções</div>
-            
-             <div class="informacoes_dados"></div>
-            <div class="informacoes_dados"></div>
-            <div class="informacoes_dados"></div>
-            <div class="informacoes_dados">
-                <img id="icones_opcoes" src="view/cms/imagem/editar.png">|
-                <img id="icones_opcoes" src="view/cms/imagem/buscar.png">|
-                <img id="icones_opcoes" src="view/cms/imagem/delete.png">
+    </div>
+    <div class="segura_tabela">
+        <div class="tabela_sobre">
+            <div class="linha_titulo">
+                <div class="col_titulo" style="width:180px; border-left:"> Titulo</div>
+                 <div class="col_titulo" style="width:280px; border-left:1px solid black;"> Texto</div>
+                 <div class="col_titulo" style="width:280px; border-left:1px solid black;"> imagem</div>
             </div>
-            
+            <div class="linha_resposta">
+                <div class="col_resposta" style="paddin-top:10px; width: 182px;">aaaaaa</div>
+                <div class="col_resposta" style="paddin-top:10px; width:280px;">aaaaaa</div>
+                <div class="col_resposta" style="paddin-top:10px; width:293px;">aaaaaa</div>
+
+            </div>
         </div>
         
-        <div class="campo_editar"> 
-            <strong>
-                Titulo: 
-            </strong>
-            
-            <input id="caixa_titulo" type="text" value="">
-            
-            <div id="caixa_descricao">
-                Descricao:
+        <div class="tabela_valores_visao">
+            <div class="linha_titulo_visao">
+                <div class="col_titulo_visao" style="width:209px; border-left:"> Visão </div>
+                <div class="col_titulo_visao" style="width:204px; border-left:1px solid black;"> Missão </div>
+                <div class="col_titulo_visao" style="width:212px; border-left:1px solid black;"> Valores </div>
             </div>
-            <textarea></textarea><br>
-             
             
-            <div id="upload"></div>
-            
-            <div id="botoes">
-                <div class="icone">
-                   <a href="#"> 
-                       <button id="salvar"><img id="salvar_icone" src="view/cms/imagem/salvar.png">Salvar </button> 
-                    </a>
+            <div class="segura_visao">
+                <h5>Titulo</h5>
+                <div class="titulo_visao"></div>
+                <div class="imagem_visao">
                 </div>
-               
-            
+                <button>Carregar foto</button>
+                <div class="texto"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies.</div>
+                <div class="segura_icones"> 
+                    <img class="icone" src="view/cms/imagem/editar.png">
+                    <img class="icone" src="view/cms/imagem/delete.png">
+                </div> 
             </div>
+            <div class="segura_visao">
+                <h5>Titulo</h5>
+                <div class="titulo_visao"></div>
+                <div class="imagem_visao">
+                </div>
+                <button>Carregar foto</button>
+                <div class="texto"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies.</div>
+                <div class="segura_icones"> 
+                    <img class="icone" src="view/cms/imagem/editar.png">
+                    <img class="icone" src="view/cms/imagem/delete.png">
+                </div> 
+            </div>
+              <div class="segura_visao">
+                <h5>Titulo</h5>
+                <div class="titulo_visao"></div>
+                <div class="imagem_visao">
+                </div>
+                <button>Carregar foto</button>
+                <div class="texto"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies.</div>
+                <div class="segura_icones"> 
+                    <img class="icone" src="view/cms/imagem/editar.png">
+                    <img class="icone" src="view/cms/imagem/delete.png">
+                </div> 
+            </div>
+            
+            
+            
         </div>
     </div>
-<!--conteudo de visao missao e valores-->
-    <div id="borda_baixo">
-        <div id="h2borda_baixo"> 
-            Missao e Valores
-        </div>
-        
-        <div class="conteudo_dentro">
-            <div class="informacoes">Titulo</div>
-            <div class="informacoes">imagem</div>
-            <div class="informacoes">descrição</div>
-            <div class="informacoes">opções</div>
-            
-             <div class="informacoes_dados"></div>
-            <div class="informacoes_dados"></div>
-            <div class="informacoes_dados"></div>
-            <div class="informacoes_dados">
-                <img id="icones_opcoes" src="view/cms/imagem/editar.png">|
-                <img id="icones_opcoes" src="view/cms/imagem/buscar.png">|
-                <img id="icones_opcoes" src="view/cms/imagem/delete.png">
-            </div>
-            
-        </div>
-        
-        <div class="campo_editar"> 
-            <strong>
-                Titulo: 
-            </strong>
-            
-            <input id="caixa_titulo" type="text" value="">
-            
-            <div id="caixa_descricao">
-                Descricao:
-            </div>
-            <textarea></textarea><br>
-             
-            
-            <div id="upload"></div>
-            
-            <div id="botoes">
-                <div class="icone">
-                   <a href="#"> 
-                       <button id="salvar"><img id="salvar_icone" src="view/cms/imagem/salvar.png">Salvar </button> 
-                    </a>
-                </div>
-               
-            
-            </div>
-        </div>
     
-    </div>
+   
     
-    
+
 
 </div>
