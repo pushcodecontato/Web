9ed3885895d1de7ac39275c600bd61fa40acbb3b
LARISSA BRUNA DE OLIVEIRA PEREIRA <17259209@CFP127ALUNO.LOCAL>
2019-05-02 09:56:37 -0300

    comitando arquivos larissa sexta feira

--- view/cms/clientes/clientes.php
+++ view/cms/clientes/clientes.php
@@ -2,43 +2,46 @@
     <h2>Clientes</h2>
 </div>
 <div id="segura_campos">
-    <div class="nomeCampos">Nome</div>
-    <div class="nomeCampos">Cpf</div>
-    <div class="nomeCampos">Email</div>
-    <div class="nomeCampos">Celular</div>
-    <div class="nomeCampos">Cidade</div>
-    <div class="nomeCampos">Estado</div>
-    <div class="nomeCampos">Status</div>
-</div>
-<div id="segura_campos">
-    <?php           require_once('controller/controllerClientes.php');
+<table width="950px">
+    <tr>
+        <td>Nome</td>
+        <td>Cpf</td>
+        <td>Email</td>
+        <td>Celular</td>
+        <td>Cidade</td>
+        <td>Estado</td>
+        <td>Status</td>
+    </tr>
+    <?php require_once('controller/controllerClientes.php');
 
-                    $controller_clientes = new ControllerClientes();
+    $controller_clientes = new ControllerClientes();
 
-                    $listRegistro =  $controller_clientes->listar_registro_clientes();
+    $listRegistro =  $controller_clientes->listar_registro_clientes();
 
 
-                    if(count($listRegistro) < 1){
-                        echo "<img class='img_not_find alt='Nada encontrado' src='view/imagem/magnify.gif'>";
-                        echo " <p class='aviso_tabela'> Nenhum registro encontrado!</p> ";
-                    }
+    if(count($listRegistro) < 1){
+        echo "<img class='img_not_find alt='Nada encontrado' src='view/imagem/magnify.gif'>";
+        echo " <p class='aviso_tabela'> Nenhum registro encontrado!</p> ";
+    }
 
-                    foreach($listRegistro as $registro){
-                ?>
-                    <div class="campos"><?=$registro->getNome()?></div>
-                    <div class="campos"><?=$registro->getCPF()?></div>
-                    <div class="campos"><?=$registro->getEmail()?></div>
-                    <div class="campos"><?=$registro->getCelular()?></div>
-                    <div class="campos"><?=$registro->getCidade()?></div>
-                    <div class="campos"><?=$registro->getUf()?></div>
-                    <img onclick="clientes_ativar_desativar(<?=@$registro->getId()?>,<?=@$registro->getStatus()?>)" 
+    foreach($listRegistro as $registro){
+    ?>
+        <tr>
+            <td><?=$registro->getNome()?></td>
+            <td><?=$registro->getCPF()?></td> 
+            <td><?=$registro->getEmail()?></td>
+            <td><?=$registro->getCelular()?></td>
+            <td><?=$registro->getCidade()?></td>
+            <td><?=$registro->getUf()?></td>
+            <td><img onclick="clientes_ativar_desativar(<?=@$registro->getId()?>,<?=@$registro->getStatus()?>)" 
                          <?php if( $registro->getStatus() == 1){ ?>
                             src="view/cms/imagem/icones/on.png" alt="on-off"
                          <?php }else{ ?>
                             src="view/cms/imagem/icones/off.png" alt="on-off"
-             >           <?php } ?>
+             >           <?php } ?></td>
+        </tr>
         <?php
-            }
-        ?>
-</div>
+    }
+    ?>
+</table>
 <link rel="stylesheet" type="text/css" href="view/cms/css/cliente.css">
