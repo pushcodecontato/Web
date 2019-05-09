1dcf07543e088e387dbe144eb8477d035b93bcd0
LUCAS SOEIRO DE SOUZA <17259214@CFP127ALUNO.LOCAL>
2019-05-09 11:16:57 -0300

    fazendo cadastrar veiculo painel de usuario

--- view/painel_usuario/veiculos/cadastrar.php
+++ view/painel_usuario/veiculos/cadastrar.php
@@ -1,8 +1,29 @@
 <!-- form que tem o Conteudo  -->
 <?php
+    
+    require_once('controller/controllerTipo_veiculo.php');
 
+    require_once('controller/controllerMarcas.php');
+
+    require_once('controller/controllerModelos.php');
+
+    $controller_tipo_veiculo = new ControllerTipoVeiculo();
+
+    $controller_marcas = new ControllerMarcas();
+
+    $controller_modelos = new ControllerModelos();
+
+    $tipo_veiculo = $controller_tipo_veiculo->listar_tipo();
+
+    $marcas = $controller_tipo_veiculo->listar_marcas();
+
+    $modelos = $controller_tipo_veiculo->listar_modelos();
+
+    $router = "";
+    
+    $funcaoJS = "tipo_veiculo_getById(id)";
 ?>
-<form class="veiculos-cadastrar">
+<form class="veiculos-cadastrar"  method="POST" id="formCadastroVeiculo" name="formCadastroVeiculo" onsubmit="<?=@$funcaoJS?>" action="<?=@$router?>" >
     <div class="veiculos-titulo">Cadastrar Veiculo</div>
     <table class="veiculos-cadastrar-table">
         <tr>
@@ -11,11 +32,16 @@
                     <tr>
                         <td>
                             <label>Tipo de veiculos</label>
+                            
                             <select id="cb_veiculos" onchange="getTipoVeiculo(this.value)">
-                                <option value="1">Carro</option>
-                                <option value="2" selected="selected">Bicicleta</option>
-                                <option value="3">Moto</option>
-                                <option value="4">Caminhão</option>
+                                <?php
+                                    $router = "router.php?controller=tipo_veiculo&modo=select";
+                                    $list_tipo =  $controller_tipo_veiculo->listar_tipo();
+
+                                    foreach($list_tipo as $registro){
+                                ?>
+                                <option value="<?=@$registro->getId()?>"><?=@$registro->getNome()?></option>   
+                                <?php }?>                             
                             </select>
                         </td>
                     </tr>
@@ -26,10 +52,15 @@
                     <tr>
                         <td> 
                             <label>Marcas</label>
-                            <select>
-                                <option>Carros</option>
-                                <option>Bicicleta</option>
-                                <option>Skate</option>
+                            <select id="cb_marcas" onchange="getMarcas(this.value)">
+                                <?php
+                                    $router = "router.php?controller=marcas&modo=select";
+                                    $list_marcas =  $controller_marcas->listar_marcas();
+
+                                    foreach($list_marcas as $registro){
+                                ?>
+                                <option value="<?=@$registro->getId()?>"><?=@$registro->getNome()?></option>   
+                                <?php }?>  
                             </select>
                         </td>
                     </tr>
